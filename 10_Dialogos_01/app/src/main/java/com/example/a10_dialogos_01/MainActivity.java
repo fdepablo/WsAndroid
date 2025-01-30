d59f5e375adfc6ecad1842c19c4df0eb33444af6
Felix de Pablo <f.depablo.lobo@gmail.com>
2025-01-30 16:33:12 +0100

    arreglos varios. El mas importante el proccessDialog ya funciona bien

--- 10_Dialogos_01/app/src/main/java/com/example/a10_dialogos_01/MainActivity.java
+++ 10_Dialogos_01/app/src/main/java/com/example/a10_dialogos_01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a10_dialogos_01;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,14 +24,15 @@ objetivo es interactuar con el usuario de alguna manera.
 Además, también se suelen usar para evitar que el usuario interaccione con la actividad
 principal mientras se cargan datos.
 
-En este ejemplo tenemos 3 tipos de dialogos:
+En este ejemplo tenemos 4 tipos de dialogos:
 
 1. Mostramos un dialogo con un solo boton, sirven para avisar a nuestro usuario
 de algo
 2. Mostramos un dialogo con dos botones, sirven para que el usuario confirme una
 accion.
 3. Mostramos un dialogo para recoger un valor y mostrarlo en actividad.
-
+4. Mostraremos un dialogo de espera. Sirve para decir al usuario que se
+esta procesando algún dato.
 Mas información: https://developer.android.com/develop/ui/views/components/dialogs?hl=es-419#java
  */
 
@@ -62,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             //Los dialogos en su manera más sencilla tiene un titulo y un mensaje
             //a mostrar
-            builder.setTitle("Gardar Datos");
+            builder.setTitle("Guardar Datos");
             builder.setMessage("Se han guardado los datos satisfactoriamente!");
             //Al menos tenemos que incluir un boton de afirmacion
             //Si no le pasamos ningun listener, por defecto cerramos el dialogo
@@ -81,12 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
             //En este caso si que voy a poner un listener al boton, ya que
             //quiero cerrar la actividad
-            builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
+            builder.setPositiveButton("Aceptar", (dialogInterface, i) -> finish());
 
             //Es este caso tambien metemos el boton en caso de que no quiera
             //hacer la accion
@@ -123,15 +120,12 @@ public class MainActivity extends AppCompatActivity {
             //Mostramos el dialogo
             progressDialog.show();
 
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            Log.i("MainActivity","Cancelando process");
-            //Cancelamos el dialogo
-            progressDialog.dismiss();
+            // Usamos un Handler para evitar bloquear el hilo principal
+            //y simular un retarde de 5 segundos
+            new Handler().postDelayed(() -> {
+                Log.i("MainActivity", "Cancelando process");
+                progressDialog.dismiss();
+            }, 5000); // 5 segundos
         });
     }
 }
